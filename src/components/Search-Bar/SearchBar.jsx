69f21b9c5b82bff04b410ed4e81a078b0ec45ace
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2024-03-31 21:23:50 +0400

    Added a price sorting on a search button click & keystroke filtering

--- src/components/Search-Bar/SearchBar.jsx
+++ src/components/Search-Bar/SearchBar.jsx
@@ -1,4 +1,29 @@
-function SearchBar() {
+import { useEffect, useState } from "react";
+import { topSellingItems } from "../../data/topSellingItems";
+import { debounce } from "../../scripts/debounce";
+
+function SearchBar({ setItems }) {
+  const [sorted, setSorted] = useState(false);
+  const [search, setSearch] = useState("");
+  const handleDefaultSort = () => {
+    if (sorted) {
+      setItems((prev) => [...prev].sort((a, b) => (a.price < b.price ? 1 : -1)));
+      setSorted(false);
+    } else {
+      setItems((prev) => [...prev].sort((a, b) => (a.price > b.price ? 1 : -1)));
+      setSorted(true);
+    }
+  };
+
+  const handleKeywordSort = (e) => {
+    const searchValue = e.target.value;
+    setSearch(searchValue);
+    const debouncedSort = debounce(() => {
+      setItems((prev) => [...prev].filter((item) => item.name.toLowerCase().includes(e.target.value.toLowerCase())));
+    });
+    e.target.value === "" ? setItems(topSellingItems) : debouncedSort();
+  };
+
   return (
     <section className="flex w-full items-center justify-center p-[2rem]">
       <div className="relative h-[4rem] w-[30rem] lg:w-[40rem]">
@@ -6,8 +31,13 @@ function SearchBar() {
           type="text"
           placeholder="Search for an item.."
           className="h-full w-full rounded-[.5rem] border-[.2rem] border-gray-400 p-[1rem_1.2rem] text-[1.2rem] text-black placeholder:text-gray-500"
+          value={search}
+          onChange={handleKeywordSort}
         />
-        <button className="absolute right-0 top-1/2 h-full translate-y-[-50%] rounded-[.5rem] bg-gray-400 p-[1rem_1.5rem]">
+        <button
+          className="absolute right-0 top-1/2 h-full translate-y-[-50%] rounded-[.5rem] bg-gray-400 p-[1rem_1.5rem]"
+          onClick={handleDefaultSort}
+        >
           <img src="/images/icons/misc/search-icon.webp " alt="" className="h-[2rem] w-[2rem] opacity-60" />
         </button>
       </div>
--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -1,13 +1,17 @@
 import SearchBar from "../components/Search-Bar/SearchBar";
 import Introduction from "../components/Hero-Page/Hero-Introduction/Introduction";
 import ItemsWrapper from "../components/Hero-Page/Hero-Top-Sellers/ItemsWrapper";
+import { topSellingItems } from "../data/topSellingItems";
+import { useState } from "react";
 
 function Hero() {
+  const [items, setItems] = useState(topSellingItems);
+
   return (
     <main className="flex w-full flex-col items-center gap-[3rem] p-[4rem_0] lg:gap-[5rem] lg:p-[8rem_0]">
-      <SearchBar />
+      <SearchBar items={items} setItems={setItems} />
       <Introduction />
-      <ItemsWrapper />
+      <ItemsWrapper itemList={items} />
     </main>
   );
 }

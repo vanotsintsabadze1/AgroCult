18914284c8281bf1aab20a546d84a4818fc909f9
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2024-05-26 23:03:59 +0400

    Deployed fix for the auth0 auto log in being used

--- src/components/Header/LoginButton.tsx
+++ src/components/Header/LoginButton.tsx
@@ -1,26 +1,10 @@
 "use client";
-import Image from "next/image";
-import { useRouter } from "next/navigation";
-
 export default function LoginButton() {
-  const router = useRouter();
-
-  function handleClick() {
-    router.push("/api/auth/login"); // TBA: Add the path to the login page
-  }
-
   return (
-    <button
-      className="flex h-[3.5rem] items-center justify-center gap-[1rem] rounded-[.5rem] bg-white px-[2rem] text-[1.5rem] font-bold  shadow-soft"
-      onClick={handleClick}
-    >
-      <p>Log In</p>
-      <Image
-        src="/images/icons/header-icons/user-profile.webp"
-        width={20}
-        height={20}
-        alt="user-profile-icon"
-      />
+    <button className="flex h-[3.5rem] items-center justify-center gap-[1rem] rounded-[.5rem] bg-white text-[1.5rem] font-bold  shadow-soft">
+      <a href="/api/auth/login" className="w-full h-full flex items-center justify-center px-[2rem]">
+        Log In
+      </a>
     </button>
   );
 }
